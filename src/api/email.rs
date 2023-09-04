75b0285bd1ecefd3cfa0ea4fed5b08bd6b6fed3b
Daniel Hartig <kingledion@protonmail.com>
2023-09-04 06:10:58 -0500

    Return send email with template to exported status

--- src/api/email.rs
+++ src/api/email.rs
@@ -1,6 +1,8 @@
 //! You'll find in email sending related endpoints.
 mod send_email;
 mod send_email_batch;
+mod send_email_with_template;
 
 pub use send_email::*;
 pub use send_email_batch::*;
+pub use send_email_with_template::*;
