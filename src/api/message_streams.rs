144f25aa59748137d04ad4181e591e17f0ab4d4b
Pierre-Alexandre St-Jean <pa@stjean.me>
2026-03-21 15:24:10 -0400

    feat add message streams list get

--- src/api/message_streams.rs
+++ src/api/message_streams.rs
@@ -5,11 +5,15 @@ use std::fmt;
 
 mod create_suppression;
 mod delete_suppression;
+mod get_message_stream;
 mod get_suppressions;
+mod list_message_streams;
 
 pub use create_suppression::*;
 pub use delete_suppression::*;
+pub use get_message_stream::*;
 pub use get_suppressions::*;
+pub use list_message_streams::*;
 
 #[derive(Debug, Clone, Serialize, Deserialize, PartialEq, Default)]
 pub enum SuppressionStatusType {
@@ -30,10 +34,61 @@ pub enum StreamIdOrName {
     StreamId(String),
 }
 
+#[derive(Debug, Clone, Serialize, Deserialize, PartialEq, Default)]
+pub enum MessageStreamType {
+    #[default]
+    Inbound,
+    Broadcasts,
+    Transactional,
+    All,
+}
+
+#[derive(Debug, Clone, Serialize, Deserialize, PartialEq, Default)]
+pub enum UnsubscribeHandlingType {
+    #[default]
+    None,
+    Custom,
+    Postmark,
+}
+
+#[derive(Debug, Clone, Serialize, Deserialize, PartialEq)]
+#[serde(rename_all = "PascalCase")]
+pub struct SubscriptionManagementConfiguration {
+    pub unsubscribe_handling_type: UnsubscribeHandlingType,
+}
+
+#[derive(Debug, Clone, Serialize, Deserialize, PartialEq)]
+#[serde(rename_all = "PascalCase")]
+pub struct MessageStream {
+    #[serde(rename = "ID")]
+    pub id: String,
+    #[serde(rename = "ServerID")]
+    pub server_id: isize,
+    pub name: String,
+    pub description: Option<String>,
+    pub message_stream_type: MessageStreamType,
+    pub created_at: String,
+    pub updated_at: Option<String>,
+    pub archived_at: Option<String>,
+    pub expected_purge_date: Option<String>,
+    pub subscription_management_configuration: SubscriptionManagementConfiguration,
+}
+
 impl fmt::Display for StreamIdOrName {
     fn fmt(&self, f: &mut fmt::Formatter) -> fmt::Result {
         match self {
             StreamIdOrName::StreamId(id) => write!(f, "{}", id),
+        }
+    }
+}
+
+impl fmt::Display for MessageStreamType {
+    fn fmt(&self, f: &mut fmt::Formatter) -> fmt::Result {
+        match self {
+            MessageStreamType::Inbound => write!(f, "Inbound"),
+            MessageStreamType::Broadcasts => write!(f, "Broadcasts"),
+            MessageStreamType::Transactional => write!(f, "Transactional"),
+            MessageStreamType::All => write!(f, "All"),
         }
     }
 }
---
+++ src/api/message_streams/list_message_streams.rs
@@ -0,0 +1,114 @@
+use std::borrow::Cow;
+
+use crate::api::message_streams::{MessageStream, MessageStreamType};
+use crate::Endpoint;
+use serde::{Deserialize, Serialize};
+use typed_builder::TypedBuilder;
+
+#[derive(Debug, Clone, PartialEq, Serialize, TypedBuilder)]
+#[serde(rename_all = "PascalCase")]
+pub struct ListMessageStreamsRequest {
+    #[serde(skip)]
+    #[builder(default, setter(into, strip_option))]
+    pub message_stream_type: Option<MessageStreamType>,
+    #[serde(skip)]
+    #[builder(default, setter(into, strip_option))]
+    pub include_archived_streams: Option<bool>,
+}
+
+#[derive(Debug, Clone, PartialEq, Serialize, Deserialize)]
+#[serde(rename_all = "PascalCase")]
+pub struct ListMessageStreamsResponse {
+    pub message_streams: Vec<MessageStream>,
+    pub total_count: isize,
+}
+
+impl Endpoint for ListMessageStreamsRequest {
+    type Request = ListMessageStreamsRequest;
+    type Response = ListMessageStreamsResponse;
+
+    fn endpoint(&self) -> Cow<'static, str> {
+        let mut query = Vec::new();
+        if let Some(message_stream_type) = &self.message_stream_type {
+            query.push(format!("MessageStreamType={message_stream_type}"));
+        }
+        if let Some(include_archived_streams) = self.include_archived_streams {
+            query.push(format!("IncludeArchivedStreams={include_archived_streams}"));
+        }
+
+        if query.is_empty() {
+            "/message-streams".into()
+        } else {
+            format!("/message-streams?{}", query.join("&")).into()
+        }
+    }
+
+    fn body(&self) -> &Self::Request {
+        self
+    }
+
+    fn method(&self) -> http::Method {
+        http::Method::GET
+    }
+}
+
+#[cfg(test)]
+mod tests {
+    use httptest::matchers::request;
+    use httptest::{responders::*, Expectation, Server};
+    use serde_json::json;
+
+    use crate::reqwest::PostmarkClient;
+    use crate::Query;
+
+    use super::*;
+
+    #[tokio::test]
+    pub async fn list_message_streams() {
+        let server = Server::run();
+
+        server.expect(
+            Expectation::matching(request::method_path("GET", "/message-streams")).respond_with(
+                json_encoded(json!({
+                    "MessageStreams": [{
+                        "ID": "outbound",
+                        "ServerID": 123457,
+                        "Name": "Transactional Stream",
+                        "Description": "This is my stream to send transactional messages",
+                        "MessageStreamType": "Transactional",
+                        "CreatedAt": "2020-07-01T00:00:00-04:00",
+                        "UpdatedAt": "2020-07-05T00:00:00-04:00",
+                        "ArchivedAt": null,
+                        "ExpectedPurgeDate": null,
+                        "SubscriptionManagementConfiguration": {
+                            "UnsubscribeHandlingType": "None"
+                        }
+                    }],
+                    "TotalCount": 1
+                })),
+            ),
+        );
+
+        let client = PostmarkClient::builder()
+            .base_url(server.url("/").to_string())
+            .build();
+
+        let req = ListMessageStreamsRequest::builder()
+            .message_stream_type(MessageStreamType::All)
+            .include_archived_streams(true)
+            .build();
+
+        assert_eq!(
+            req.endpoint(),
+            "/message-streams?MessageStreamType=All&IncludeArchivedStreams=true"
+        );
+
+        let resp = req
+            .execute(&client)
+            .await
+            .expect("Should get a response and be able to json decode it");
+
+        assert_eq!(resp.total_count, 1);
+        assert_eq!(resp.message_streams[0].id, "outbound");
+    }
+}
